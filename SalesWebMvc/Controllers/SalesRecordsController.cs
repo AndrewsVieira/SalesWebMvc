e1bec6491b04a62def020746273f5f46ac109e5d
Andrews <andrews.a.vieira@gmail.com>
2022-01-09 11:18:02 -0300

    Bug no GroupingSearch

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,14 +37,32 @@ namespace SalesWebMvc.Controllers
 
         public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            if (!minDate.HasValue)
-                minDate = new DateTime(DateTime.Now.Year, 1, 1);
-            if (!maxDate.HasValue)
-                maxDate = DateTime.Now;
-            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
-            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
-            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
-            return View(result);
+            try
+            {
+                if (!minDate.HasValue)
+                    minDate = new DateTime(DateTime.Now.Year, 1, 1);
+                if (!maxDate.HasValue)
+                    maxDate = DateTime.Now;
+                ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+                ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+                var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+                return View(result);
+            }
+            catch
+            {
+                string message = "Página temporariamente indisponível.";
+                return RedirectToAction(nameof(Error), new { message = message });
+            }
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
